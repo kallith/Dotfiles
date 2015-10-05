7f03877a0ea2acd55ce0e63803bce18f6d710f87
kallith <karlt13@ru.is>
2015-10-05 11:03:55 +0000

    Fixed small problems with the hidpi patch

--- apply_no_hidpi.sh
+++ apply_no_hidpi.sh
@@ -3,7 +3,7 @@ git apply --check no_hidpi.patch
 while true; do
     read -p "Do you wish to apply this patch[y/n]?" yn
     case $yn in
-        [Yy]* ) git apply --stat no_hidpi.patch
+        [Yy]* ) git apply --stat no_hidpi.patch; break;;
         [Nn]* ) exit;;
         *) echo "Please answer y/n.";;
     esac
