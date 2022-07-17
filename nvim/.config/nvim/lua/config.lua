b9edb92ae614abcdcf2ea4ac6b6038ea79a297a5
kallith <--global>
2022-07-17 17:28:42 +0000

    setup common LS

--- nvim/.config/nvim/lua/config.lua
+++ nvim/.config/nvim/lua/config.lua
@@ -1,32 +1,28 @@
-require('nvim-treesitter.configs').setup {
-    -- A list of parser names, or "all"
-    ensure_installed = { "python", "lua", "bash", "fish" },
-
-    -- Install parsers synchronously (only applied to `ensure_installed`)
+require'nvim-treesitter.configs'.setup{
+    ensure_installed = "all",
     sync_install = false,
-
-    -- Automatically install missing parsers when entering buffer
     auto_install = true,
-
-    -- List of parsers to ignore installing (for "all")
-    ignore_install = { "javascript" },
-
+    ignore_install = {},
     highlight = {
-        -- `false` will disable the whole extension
         enable = true,
-
-        -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-        -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-        -- the name of the parser)
-        -- list of language that will be disabled
-        -- disable = { "c", "rust" },
-
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        -- additional_vim_regex_highlighting = false,
+        disable = {},
+        additional_vim_regex_highlighting = false,
     },
 }
-require('nvim-lsp-installer').setup {}
-require('lspconfig').pyright.setup {}
+
+require'nvim-lsp-installer'.setup{}
+
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+--
+-- bash
+require'lspconfig'.bashls.setup{}
+-- latex markdown and more
+require'lspconfig'.ltex.setup{}
+-- python
+require'lspconfig'.pyright.setup{}
+-- sql
+require'lspconfig'.sqlls.setup{}
+-- lua
+require'lspconfig'.sumneko_lua.setup{}
+-- vim
+require'lspconfig'.vimls.setup{}
