9554152321434c649d10fcacb53121cbb7d8628f
kallith <--global>
2022-07-17 16:10:12 +0000

    fix reference to xkb file

--- is-colemak/bin/add_is_colemak.sh
+++ is-colemak/bin/add_is_colemak.sh
@@ -3,5 +3,5 @@ cd $(dirname "$0")/..
 
 sudo patch -u /usr/share/X11/xkb/rules/evdev.xml -i evdev.xml.patch
 sudo patch -u /usr/share/X11/xkb/rules/base.xml -i base.xml.patch
-cat usr.share.X11.xkb | sudo tee -a /usr/share/X11/xkb/symbols/is
+cat  xkb.symbols.is.colemak | sudo tee -a /usr/share/X11/xkb/symbols/is
 
