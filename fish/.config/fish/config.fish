d745c5c0cc2f839070c57c4ada1f92f98482c22c
kallith <--global>
2022-07-17 16:10:58 +0000

    startx from fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,7 +2,12 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set -gx EDITOR vim
-
 status is-login; and pyenv init --path | source
 status is-interactive; and pyenv init - | source
+
+# Start X at login
+if status is-login
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+        exec startx -- -keeptty
+    end
+end
