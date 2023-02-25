feaf0a579e3e0f93f3e566455bf420a5279a8e2c
kallith <5684942+kallith@users.noreply.github.com>
2023-02-25 18:20:24 +0000

    1 instead of yes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -16,5 +16,5 @@ set -g __fish_git_prompt_color_cleanstate brgreen
 set -g __fish_git_prompt_color_invalidstate red
 set -g __fish_git_prompt_color_stagedstate yellow
 set -g __fish_git_prompt_show_informative_status 1
-set -g __fish_git_prompt_showdirtystate "yes"
+set -g __fish_git_prompt_showdirtystate 1
 set -g __fish_git_prompt_showupstream "informative"
