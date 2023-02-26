fb90285a43c3264e8dbde7bc047080568dedb371
kallith <5684942+kallith@users.noreply.github.com>
2023-02-26 11:48:32 +0000

    use vi keys in info

--- dotfiles/fish/.config/fish/conf.d/envvars.fish
+++ dotfiles/fish/.config/fish/conf.d/envvars.fish
@@ -1,5 +1,5 @@
 # Use bat as man pager.
-set -xU MANPAGER "sh -c 'col -bx | bat -l man --plain --paging=always'"
+set -Ux MANPAGER "sh -c 'col -bx | bat -l man --plain --paging=always'"
 # Disable fish greeting
 set -U fish_greeting
-
+set -Ux EDITOR nvim
---
+++ dotfiles/fish/.config/fish/functions/info.fish
@@ -0,0 +1,4 @@
+function info --description 'alias info info --vi-keys'
+ command info --vi-keys $argv
+        
+end
