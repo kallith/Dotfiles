0cc88ef34be8131067efae178345a7ae897630f9
kallith <5684942+kallith@users.noreply.github.com>
2026-06-09 15:17:27 +0000

    use vim by default not nvim

--- dotfiles/fish/.config/fish/conf.d/envvars.fish
+++ dotfiles/fish/.config/fish/conf.d/envvars.fish
@@ -1,6 +1,4 @@
-# Use bat as man pager.
-set -Ux MANROFFOPT "-c" 
-set -Ux MANPAGER "sh -c 'col -bx | bat -l man --plain --paging=always'"
+set -Ux MANPAGER 'bat -plman --paging=always'
 # Disable fish greeting
 set -U fish_greeting
-set -Ux EDITOR nvim
+set -Ux EDITOR vim
--- dotfiles/fish/.config/fish/config.fish
+++ dotfiles/fish/.config/fish/config.fish
@@ -1,10 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-end
-
-# Start X at login
-if status is-login
-    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-        exec startx -- -keeptty
-    end
+else
+    
 end
